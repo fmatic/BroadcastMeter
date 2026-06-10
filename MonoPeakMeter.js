6640056a11fec488956c67a5cc6467ee3a9f1a22
fmatic <fmatic@gmail.com>
2026-06-10 12:38:47 +0300

    Initial alpha release of Mono Peakmeter plugin

---
+++ MonoPeakMeter.js
@@ -0,0 +1,10 @@
+var pluginConfig = {
+    name: 'Mono Peakmeter',
+    version: '0.2.0',
+    author: 'JanneDX',
+    frontEndPath: 'MonoPeakMeter/monopeakmeter.js'
+}
+
+module.exports = {
+    pluginConfig
+}
